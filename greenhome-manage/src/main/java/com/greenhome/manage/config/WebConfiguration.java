138127aa6690294eec3c2f3b9481bb60e61246fa
林磊 <linlei@linleideMacBook-Pro.local>
2022-06-23 10:34:00 +0800

    增加聚合接口测试

--- greenhome-manage/src/main/java/com/greenhome/manage/config/WebConfiguration.java
+++ greenhome-manage/src/main/java/com/greenhome/manage/config/WebConfiguration.java
@@ -1,7 +1,27 @@
-package com.greenhome.manage.config;/**
+package com.greenhome.manage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
  * @Author smart_joker
  * @Date 2022/6/23 9:43 上午
- * @Description 
+ * @Description 处理 spring 框架提供的转换器
+ * 解决StringHttpMessageConverter.getContentLength，ClassCastException
  * @Version 1.0
- */public class WebConfiguration {
+ */
+
+@Configuration
+public class WebConfiguration  implements WebMvcConfigurer {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 第一种方式是将 json 处理的转换器放到第一位，使得先让 json 转换器处理返回值，这样 String转换器就处理不了了。
+       converters.add(0, new MappingJackson2HttpMessageConverter());
+        // 第二种就是把String类型的转换器去掉，不使用String类型的转换器
+        //converters.removeIf(httpMessageConverter -> httpMessageConverter.getClass() == StringHttpMessageConverter.class);
+    }
 }
--- greenhome-manage/src/main/java/com/greenhome/manage/controller/ThreadBaseController.java
+++ greenhome-manage/src/main/java/com/greenhome/manage/controller/ThreadBaseController.java
@@ -1,7 +1,31 @@
-package com.greenhome.manage.controller;/**
+package com.greenhome.manage.controller;
+
+import com.greenhome.service.inter.thread.ThreadBaseService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.ExecutionException;
+
+/**
  * @Author smart_joker
  * @Date 2022/6/22 4:40 下午
- * @Description 
+ * @Description 接口聚合获取
  * @Version 1.0
- */public class ThreadBaseController {
+ */
+@RestController
+@Api(tags = "接口聚合获取测试")
+@RequestMapping("/thread")
+public class ThreadBaseController  {
+
+    @Autowired
+    public ThreadBaseService threadBaseService;
+
+    @ApiOperation(value = "测试聚合接口开发", notes = "")
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public String doTest() throws ExecutionException, InterruptedException {
+        return threadBaseService.doTest();
+    }
 }
